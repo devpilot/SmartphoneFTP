880fbfae266a95f6d04425b5d27f80a31f20b2da
Aritra <aet419@gmail.com>
2013-06-15 15:45:14 +0530

    getter/setter for account related tasks

---
+++ src/vu/smartphoneftp/Server.java
@@ -0,0 +1,59 @@
+package vu.smartphoneftp;
+
+public class Server {
+	private int _id, port;
+	private String title, host, username, password;
+	
+	public int get_id() {
+		return _id;
+	}
+	
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getHost() {
+		return host;
+	}
+	
+	public void setHost(String host) {
+		this.host = host;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return getTitle();
+	}
+}
