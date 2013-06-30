b8c49843bb4aa11a1dcad0f4ea762c9f9ff2dffe
Aritra <aet419@gmail.com>
2013-06-30 08:09:33 +0530

    removed extra button, place the uttons in right order, changed activity title, set buttons click handeler

--- src/vu/smartphoneftp/ConnectionEdit_Activity.java
+++ src/vu/smartphoneftp/ConnectionEdit_Activity.java
@@ -2,14 +2,39 @@ package vu.smartphoneftp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ConnectionEdit_Activity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_conection_edit);
+		
+		// getting component reference
+		Button btnsave = (Button) findViewById(R.id.btnSave);
+		Button btnCancel = (Button) findViewById(R.id.btnCancel);
+		
+		// Save button click action
+		btnsave.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		// Cancel button click action
+		btnCancel.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
 	}
-
 }
