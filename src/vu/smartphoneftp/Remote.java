66b81f1464199f04f824199a18e9dce6f742407c
Aritra <aet419@gmail.com>
2013-07-23 01:21:22 +0530

    renamed connect() to connectServer() placed connectServer and getList in asyncTask ftpclient field change to static for holding reference added network check method

--- src/vu/smartphoneftp/Remote.java
+++ src/vu/smartphoneftp/Remote.java
@@ -8,6 +8,9 @@ import org.apache.commons.net.ftp.FTPReply;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -15,7 +18,8 @@ public class Remote {
 
 	private static final String TAG = "FTP";
 	private Context context;
-	private final FTPClient ftp = new FTPClient();
+	private static FTPClient ftp;
+	private static Server server;
 
 	/**
 	 * @param context
@@ -23,83 +27,118 @@ public class Remote {
 	public Remote(Context context) {
 		this.context = context;
 	}
-	
-	public void connect(final Server server) {
-		
-		AsyncTask<Void, Void, Boolean> task = new AsyncTask<Void, Void, Boolean>() {
-			String msg;
-			@Override
-			protected Boolean doInBackground(Void... params) {
-				ftp.setConnectTimeout(500);
-				try {
-					int reply;
-					ftp.connect(server.getHost(), server.getPort());
-					reply = ftp.getReplyCode();
-					if (!FTPReply.isPositiveCompletion(reply)) {
-						ftp.disconnect();
-						Log.e(TAG, "FTP server refused connection.");
-					}
-					Log.d(TAG, "Connected to server");
-					if (!ftp.login(server.getUsername(), server.getPassword())) {
-						msg = ftp.getReplyString();
-						Log.d(TAG, ftp.getReplyString());
-						ftp.logout();
-						return false;
-					}
-					// should be connected here
-					Log.d(TAG, ftp.getReplyString());
-					return true;
-				} catch (IOException e) {
-					if (ftp.isConnected()) {
-		                try {
-		                    ftp.disconnect();
-		                } catch (IOException f) {
-		                    // do nothing
-		                }
-		            }
-					msg = "Could not connect to server";
-					Log.e(TAG, e.getMessage());
-				}
-				return false;
-			}
 
-			@Override
-			protected void onPostExecute(Boolean result) {
-				super.onPostExecute(result);
-				Log.d(TAG, String.valueOf(result));
-				if(!result){
-					showAlert(msg);
+	public void connectServer(final Server server, final boolean reconnect) {
+		if (isNetworkAvailable()) {
+			Remote.server = server;
+			ftp = new FTPClient();
+			new AsyncTask<Void, Void, Boolean>() {
+				String msg; // warning message for dialog
+				
+				@Override
+				protected Boolean doInBackground(Void... params) {
+					ftp.setConnectTimeout(500);
+					try {
+						int reply;
+						ftp.connect(server.getHost(), server.getPort());
+						reply = ftp.getReplyCode();
+						if (!FTPReply.isPositiveCompletion(reply)) {
+							ftp.disconnect();
+							Log.e(TAG, "FTP server refused connection.");
+						}
+						Log.d(TAG, "Connected to server");
+						if (!ftp.login(server.getUsername(),
+								server.getPassword())) {
+							msg = ftp.getReplyString();
+							Log.d(TAG, ftp.getReplyString());
+							ftp.logout();
+							return false;
+						}
+						// should be connected here
+						Log.d(TAG, ftp.getReplyString());
+						return true;
+					} catch (IOException e) {
+						if (ftp.isConnected()) {
+							try {
+								ftp.disconnect();
+							} catch (IOException f) {
+								// do nothing
+							}
+						}
+						msg = "Could not connect to server";
+						Log.e(TAG, e.getMessage());
+					}
+					return false;
 				}
-			}
-		};
-		task.execute();
+				
+				@Override
+				protected void onPostExecute(Boolean result) {
+					super.onPostExecute(result);
+					Log.d(TAG, String.valueOf(result));
+					if (!result) {
+						showAlert(msg);
+					} else if (!reconnect) {
+						Intent i = new Intent("vu.smartphoneftp.FileBrowser_Activity");
+						context.startActivity(i);
+					}
+				}
+			}.execute();
+		} else {
+			showAlert("Not connected to internet");
+		}
 	}
 
-	 private void showAlert(String message){
-		 // building dialog
-		 AlertDialog ad = new AlertDialog.Builder(context).create();
-		 ad.setIcon(android.R.drawable.ic_dialog_alert);
-		 ad.setTitle("Error");
-		 ad.setMessage(message);
-		 ad.show();
-	 }
-
 	public void getList() {
-		Runnable runnable = new Runnable() {
-			public void run() {
+		new AsyncTask<Void, Void, FTPFile[]>(){
+			@Override
+			protected FTPFile[] doInBackground(Void... params) {
 				try {
 					// enter passive mode
 					ftp.enterLocalPassiveMode();
-					FTPFile[] f = ftp.listFiles();
-					for (FTPFile ftpFile : f) {
-						Log.d(TAG, ftpFile.toFormattedString());
-					}
+					return ftp.listFiles();
 				} catch (IOException e) {
 					Log.e(TAG, e.getMessage());
+					connectServer(server, true);
+					getList();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(FTPFile[] result) {
+				super.onPostExecute(result);
+				if(result != null){
+					for (FTPFile ftpFile : result) {
+						Log.d(TAG, ftpFile.toFormattedString());
+					}
 				}
 			}
-		};
-		new Thread(runnable).start();
+		}.execute();
+	}
 
+	private void showAlert(String message) {
+		// building dialog
+		AlertDialog ad = new AlertDialog.Builder(context).create();
+		ad.setIcon(android.R.drawable.ic_dialog_alert);
+		ad.setTitle("Error");
+		ad.setMessage(message);
+		ad.show();
+	}
+
+	/**
+	 * Check network connection availability
+	 * 
+	 * @return boolean
+	 */
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		// if no network is available networkInfo will be null
+		// otherwise check if we are connected
+		if (networkInfo != null && networkInfo.isConnected()) {
+			return true;
+		}
+		return false;
 	};
 }
